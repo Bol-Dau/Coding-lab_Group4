1a76725c58dae57f7e88e9327f32c949d9aa19f6
Bol <b.dau@alustudent.com>
2025-10-07 06:00:52 -0700

    Rearranged the structure of the directory named hospital_data Changed some codes in the file called analyze_logs.sh such that it can print the total count per device and time stamp for the fist and last entry

--- hospital_data/analyze_log.sh
+++ hospital_data/analyze_log.sh
@@ -4,23 +4,23 @@
 
 REPORT_DIR="reports"
 REPORT_FILE="$REPORT_DIR/analysis_report.txt"
-LOG_DIR="hospital_data/active_logs"
+LOG_DIR="active_logs"
 
 # Ensure reports directory exists
 mkdir -p "$REPORT_DIR"
 
 # Interactive prompt
 echo "Select log file to analyze:"
-echo "1) Heart Rate (heart_rate.log)"
-echo "2) Temperature (temperature.log)"
-echo "3) Water Usage (water_usage.log)"
+echo "1) Heart Rate (heart_rate_log.log)"
+echo "2) Temperature (temperature_log.log)"
+echo "3) Water Usage (water_usage_log.log)"
 read -p "Enter choice (1-3): " choice
 
 #validate choice and map to file
 case "$choice" in
-    1) LOG_FILE="$LOG_DIR/heart_rate.log" ;;
-    2) LOG_FILE="$LOG_DIR/temperature.log" ;;
-    3) LOG_FILE="$LOG_DIR/water_usage.log" ;;
+    1) LOG_FILE="$LOG_DIR/heart_rate_log.log" ;;
+    2) LOG_FILE="$LOG_DIR/temperature_log.log" ;;
+    3) LOG_FILE="$LOG_DIR/water_usage_log.log" ;;
     *) echo "Invalid choice. Please run again with option 1, 2, or 3."
        exit 1 ;;
 esac
@@ -34,10 +34,11 @@ fi
 # Analysis
 # Assumes log format: "timestamp device_name value"
 # Example: 2025-10-04T10:30:00 DeviceA 85
-DEVICE_COUNTS=$(awk '{print $2}' "$LOG_FILE" | sort | uniq -c)
+DEVICE_COUNTS=$(awk '{print $3}' "$LOG_FILE" | sort | uniq -c | awk '{print $2": "$1}'
+)
 
-FIRST_TS=$(head -n 1 "$LOG_FILE" | awk '{print $1}')
-LAST_TS=$(tail -n 1 "$LOG_FILE" | awk '{print $1}')
+FIRST_TS=$(head -n 1 "$LOG_FILE" | awk '{print $2}')
+LAST_TS=$(tail -n 1 "$LOG_FILE" | awk '{print $2}')
 
 # Append to report
 {
