16bc0eb0f5ccc4f18034ea3d1fdc0254f539bea5
nshutilancelot3 <nshutilancelot111@gmail.com>
2025-10-09 03:39:35 -0700

    logs

--- analyze_log.sh
+++ analyze_log.sh
@@ -1,32 +1,4 @@
-#!/bin/bash
-# Task 2:Intelligent Analysis Script
-# This script analyzes selected log files and appends results to reports/analysis_report.txt 
 
-# Defining the variables for the reports directory, analysis file, and the path for the log files to be analyzed. 
-REPORT_DIR="hospital_data/reports"
-REPORT_FILE="$REPORT_DIR/analysis_report.txt"
-LOG_DIR="hospital_data/active_logs"
-CONTINUATION="y"
-
-# Making sure that reports directory exisits.
-mkdir -p "$REPORT_DIR"
-
-<<<<<<< HEAD
-# Interactive prompt
-echo "Select log file to analyze:"
-echo "1) Heart Rate (heart_rate.log)"
-echo "2) Temperature (temperature.log)"
-echo "3) Water Usage (water_usage.log)"
-read -p "Enter choice (1-3): " choice
-
-#validate choice and map to file
-case "$choice" in
-    1) LOG_FILE="$LOG_DIR/heart_rate.log" ;;
-    2) LOG_FILE="$LOG_DIR/temperature.log" ;;
-    3) LOG_FILE="$LOG_DIR/water_usage.log" ;;
-    *) echo "Invalid choice. Please run again with option 1, 2, or 3."
-       exit 1 ;;
-esac
 
 # Check if log file exists
 if [ ! -f "$LOG_FILE" ]; then
